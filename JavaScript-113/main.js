0f92f46e44aa40fb59c216c99ec91fc36491adff
RanzyLetov <viktorkravcenko171@gmail.com>
2025-02-25 19:22:16 +0700

    Add files via upload

---
+++ JavaScript-113/main.js
@@ -0,0 +1,29 @@
+const btnOpenModal = document.querySelector('.btn-open')
+const modal = document.querySelector('.modal')
+const body = document.body
+
+const openModal = () => {
+    modal.classList.add('modal--open')
+    body.classList.add('body--fixed')
+}
+const clossModal = () => {
+    modal.classList.remove('modal--open')
+    body.classList.remove('body--fixed')
+}
+
+btnOpenModal.addEventListener('click', () => {
+    openModal()
+})
+
+modal.addEventListener('click', event => {
+    const target = event.target
+    if ((target && target.classList.contains('modal')) || target.classList.contains('modal__close-btn')) {
+        clossModal()
+    }
+})
+
+document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+        clossModal()
+    }
+})
